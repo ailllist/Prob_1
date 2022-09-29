af28529d5ea863f327aa0dd6aad874aab3d08342
Lee Hojun <hojun060534@gmail.com>
2022-09-29 15:03:23 +0900

    add_poisson
    first ver. approximate GT data

--- Poisson.m
+++ Poisson.m
@@ -1,10 +1,14 @@
+alp = 2;
 n = 100000;
-p = 0.0001;
-tot = 1000;
+p = alp/n;
+tot = 10000;
 
 counting_arr = zeros(1, n);
 avg_arr = zeros(1, n);
 CDF_arr = zeros(1, n);
+GT = zeros(1, n);
+
+zero_cnt = 0;
 
 for i = 1:tot
     count_sel = 0;
@@ -14,7 +18,12 @@ for i = 1:tot
         else
         end
     end
-    counting_arr(count_sel) = counting_arr(count_sel) + 1;
+    if count_sel ~= 0    
+        counting_arr(count_sel) = counting_arr(count_sel) + 1;
+    else
+        zero_cnt = zero_cnt + 1;
+    end
+    i
 end
 
 for i = 1:length(avg_arr)
@@ -27,8 +36,16 @@ for i = 1:length(avg_arr)
     end
 end
 
+for i = 1:length(GT)
+    GT(i) = (alp^i)*((exp(-alp))/factorial(i));
+end
+
 figure(1)
 stem(avg_arr, "r")
-title("PMF, Binomial")
+title("PMF, Poisson")
 xlim([-10, 100])
 ylim([-0.1, 0.2])
+
+figure(2)
+stairs(CDF_arr)
+title("CDF, Poisson")
